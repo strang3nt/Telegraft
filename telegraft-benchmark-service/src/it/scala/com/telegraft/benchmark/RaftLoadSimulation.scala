2b49f9b510877a23d628338c9ce5ea26731a2724
Alessandro Flori <alessandro.flori@proton.me>
2022-12-10 17:40:25 +0100

    Working example

--- telegraft-benchmark-service/src/it/scala/com/telegraft/benchmark/RaftLoadSimulation.scala
+++ telegraft-benchmark-service/src/it/scala/com/telegraft/benchmark/RaftLoadSimulation.scala
@@ -1,28 +1,34 @@
 package com.telegraft.benchmark
 
 import com.github.phisgr.gatling.grpc.Predef._
-import com.telegraft.rafktor.proto.Rafktor.{ClientRequest, LogEntryPayload}
+import com.telegraft.rafktor.proto.Rafktor.{ ClientRequest, LogEntryPayload }
 import io.gatling.core.Predef._
 import com.telegraft.rafktor.proto.RafktorClient.TelegraftRaftClientServiceGrpc
 import com.telegraft.rafktor.proto.RafktorClient.ClientRequestPayload
 import com.telegraft.statemachine.proto.TelegraftStateMachine.CreateUserRequest
 
-import scala.concurrent.duration._
-
 class RaftLoadSimulation extends Simulation {
 
-  val grpcConf = grpc(managedChannelBuilder("0.0.0.0", 8350))
-  val request = grpc("request_1")
-    .rpc(TelegraftRaftClientServiceGrpc.METHOD_CLIENT_REQUEST)
-    .payload(ClientRequestPayload("clientId", "requestId", Some(LogEntryPayload(LogEntryPayload.Payload.CreateUser(CreateUserRequest("NewUser"))))))
-    //.extract(_.)(_ is SERVING)
+  val grpcConf = grpc(managedChannelBuilder("localhost", 8350).usePlaintext())
+
+  val requestWithRepetition =
+    repeat(10) {
+      exec(
+        grpc("create_users")
+          .rpc(TelegraftRaftClientServiceGrpc.METHOD_CLIENT_REQUEST)
+          .payload(
+            ClientRequestPayload(
+              "clientId",
+              "requestId",
+              Some(LogEntryPayload(LogEntryPayload.Payload.CreateUser(CreateUserRequest("NewUser"))))))
+          .extract(_.payload.some)(x => x.saveAs("LOG_ENTRY_RESPONSE"))).exec(session => {
+        println("This is the variable: " + session("LOG_ENTRY_RESPONSE").as[String])
+        session
+      })
+    }
 
   val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(request)
-    .pause(7.seconds)
-    .exec(request)
-    .exec(request)
-    .exec(request)
+    .exec(requestWithRepetition)
 
   setUp(scn.inject(atOnceUsers(1)).protocols(grpcConf))
 }
